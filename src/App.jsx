d6c89a215a2bc36821cbbff64d9af85d25a1c38b
Eddie Jacobian <edjacobian@gmail.com>
2023-07-26 16:47:05 -0400

    added contactRowjsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,11 @@
+import ContactList from './components/ContactList';
+import React from 'react';
 import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <ContactList />
+    </>
+  );
+}
